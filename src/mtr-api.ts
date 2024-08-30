d0ce4e9380bf5d01542182e72f8f72809751555f
Dennis Chan <dennisming2012@gmail.com>
2024-08-30 22:57:41 +0800

    chore: add constant

--- src/mtr-api.ts
+++ src/mtr-api.ts
@@ -2,9 +2,10 @@ import axios from 'axios';
 import { BusResponse } from './types';
 
 const API_BASE_URL = 'https://rt.data.gov.hk/v1/transport/mtr';
+const BUS_SCHEDULE = '/bus/getSchedule';
 
 async function fetchMtrBus(language: 'en' | 'zh', routeName: string) {
-  return axios.post<BusResponse>(`${API_BASE_URL}/bus/getSchedule`, {
+  return axios.post<BusResponse>(`${API_BASE_URL}${BUS_SCHEDULE}`, {
     language,
     routeName,
   });
